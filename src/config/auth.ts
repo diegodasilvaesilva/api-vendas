5b3afbe01906e4f1f97c810fef3bb5d8907ed3ba
Diego da Silva e Silva <diego@gam.com.br>
2021-01-24 09:15:40 -0300

    Reinstall eslint, prettier and fix auth.ts

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: process.env.APP_SECRET,
+    secret: process.env.APP_SECRET || '',
     //secret: '5bfd88083b060eedb6a06bbd58130e6d',
     expiresIn: '1d'
   }
